786ca58cd42a029fec9d349d52a8956340c5c4d0
Alex Floyd <alexfloydmusic2@gmail.com>
2025-12-19 22:20:10 -0500

    updated schedule

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,27 +1,35 @@
 function Schedule() {
     return (
       <div className="text-left">
-        <div id="august-2025">
-        <h2 className="bottom-line">August 2025</h2>          
-          {/* COPY HERE */}
-          <h3>Saturday, August 16, 2025, 5:30pm - 8:30pm</h3>
-          <p>with The Sun Seakers
-          <br />John Scott's Surf Shack
-            <br />
-          Westhampton Beach, NY 11978</p>
-          <p className="line-divide"></p>
-          {/* COPY HERE */}          
-          <h3>Sunday, August 17, 2025, 2:00pm - 5:00pm</h3>
-          <p>with The Sun Seakers
-          <br />Lovefins at Watch Hill
-            <br />
-          Patchogue, NY 11772</p>
-          <p className="line-divide"></p>
-          <h3>Sunday, August 24, 2025</h3>
-          <p>with The Sun Seakers
-          <br />Marlena's Pack Out
-            <br />
-          Montauk, NY 11954</p>
+        <div id="january-2026">
+          <h2 className="bottom-line">January 2026</h2>          
+            {/* COPY HERE */}
+            <h3>Saturday, January 3, 2026</h3>
+            <p>with Joey Ama Dio
+              <br />Shrine World Music Venue
+              <br />2271 Adam Clayton Powell Jr Blvd
+              <br />New York, NY 10030
+            </p>
+            <p className="line-divide"></p>
+            {/* COPY HERE */}
+            <h3>Monday, January 12, 2026</h3>
+            <p>with Joey Ama Dio
+              <br />Cowgirl Seahorse
+              <br />259 Front Street
+              <br />New York, NY 10038
+            </p>
+            <p className="line-divide"></p>
+        </div>
+        <div id="february-2026">
+          <h2 className="bottom-line">February 2026</h2>
+            {/* COPY HERE */}
+            <h3>Saturday, February 14, 2026</h3>
+            <p>with Carnival Dogs
+              <br />Beau's Bar
+              <br />54 Broadway
+              <br />Greenlawn, NY 11740
+            </p>
+            <p className="line-divide"></p>      
         </div>
       </div>
     );
